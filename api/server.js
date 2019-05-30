4266bedb6c32723ddf2f40f25ebc22fb2e2192ed
Spencer Meredith <16077691+Spencer-M24@users.noreply.github.com>
2019-05-30 16:37:13 -0700

    Update server.js

--- api/server.js
+++ api/server.js
@@ -3,11 +3,13 @@ const helmet = require('helmet');
 const morgan = require('morgan');
 const cors = require('cors');
 
+
+
 const server = express();
 
 server.use(express.json());
 server.use(helmet());
-server.use(morgan());
+server.use(morgan('dev'));
 server.use(cors());
 
 server.get('/', (req, res) => {
